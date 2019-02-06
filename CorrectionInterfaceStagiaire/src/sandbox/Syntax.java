41d9e8f3ae0542d0944431b54dd8cb42237836e9
Milena <milena.chaine@gmail.com>
2019-02-06 15:39:18 +0100

    regexp+récursivité

--- CorrectionInterfaceStagiaire/src/sandbox/Syntax.java
+++ CorrectionInterfaceStagiaire/src/sandbox/Syntax.java
@@ -8,6 +8,25 @@ import primer.EtudiantIM;
 import primer.EtudiantTrad;
 
 public class Syntax {
+	
+	public static String reverse(String s) {
+		String txt = s;
+		String[] tab = txt.split("");
+		String resultat = "";
+        for (int i = tab.length - 1; i >= 0; i--) {
+        	resultat = resultat.concat(tab[i]);
+        }
+		return resultat;
+	}
+	
+	//version récursive !
+	public static String reverse2(String s) {
+		String txt = s;
+		if (txt.isEmpty()) {
+			return txt;
+		}
+		return reverse2(txt.substring(1)) + txt.substring(0,1);
+	}
 
 	public static void main(String[] args) {
 		System.out.println("Tests!");
@@ -49,7 +68,17 @@ public class Syntax {
         }
         
         //coder une méthode (récursive) String reverse(String s) qui renvoie s dans l'ordre inverse e.g. reverse(« abc ») → « cba »
-        //TODO;
+        String palindrome = "kayak";
+        String not_palindrome = "abc";
+        
+        System.out.println(palindrome + " : " + reverse(palindrome));
+        System.out.println(not_palindrome + " : " + reverse(not_palindrome));
+        
+        String palindrome2 = "kayak";
+        String not_palindrome2 = "abcdefgh";
+        
+        System.out.println(palindrome + " : " + reverse2(palindrome2));
+        System.out.println(not_palindrome + " : " + reverse2(not_palindrome2));
         
         //types paramétrés
 		EtudiantErTim[] promo = new EtudiantErTim[5];
@@ -96,15 +125,11 @@ public class Syntax {
 
 		}
 		
-		//0) Extraire la valeur de Pi mentionnée dans la chaîne
-		//1) Effectuez les divisions mentionnées et afficher les résultats correspondants.
-		//2) Récupérez les noms propres
-		//3) Récupérez les numéros de siècle
 		String pi = "Pi dont la valeur avec 6 décimales est 3.141592 peut s'approximer avec la série de fractions " + 
 				"suivantes : 3/1 , 22/7, 333/106, 355/113, 103993/33102. Al-Khawarizmi, au IXe siècle, est " + 
 				"persuadé que PI est irrationnel. Moïse Maïmonide fait également état de cette idée durant le XIIe " + 
 				"siècle";
-		//0)
+		//0) Extraire la valeur de Pi mentionnée dans la chaîne
 		Pattern findpi = Pattern.compile("\\d\\.\\d+");
 		Matcher fp = findpi.matcher(pi);
 		while (fp.find()) {
@@ -112,6 +137,34 @@ public class Syntax {
 			int to = fp.end();
 			System.out.println("Pi : " + pi.substring(from,to));
 		}
+		
+		//1) Effectuez les divisions mentionnées et afficher les résultats correspondants.
+		Pattern divisions = Pattern.compile("(\\d+)/(\\d+)");
+		Matcher div = divisions.matcher(pi);
+		while (div.find()) {
+			int from = div.start();
+			int to = div.end();
+			System.out.println("Division : " + pi.substring(from,to) + " -- Résultat : " + Integer.valueOf(div.group(1))/Integer.valueOf(div.group(2)));
+		}
+		
+		//2) Récupérez les noms propres
+		// qu'on définit par au moins deux tokens commençant par une majuscule mais non entièrement en majuscules, séparés par une espace ou un tiret
+		Pattern findnp = Pattern.compile("[A-Z][^A-Z ]+( |-)[A-Z][^A-Z \\p{Punct}]+");
+		Matcher fnp = findnp.matcher(pi);
+		while (fnp.find()) {
+			int from = fnp.start();
+			int to = fnp.end();
+			System.out.println("Nom : " + pi.substring(from,to));
+		}
+		
+		//3) Récupérez les numéros de siècle
+		//càd ces lettres en majuscule : I, V, X (suffisant pour les siècles ici)
+		Pattern findcenturies = Pattern.compile("[XIV]{2,}");
+		Matcher fc = findcenturies.matcher(pi);
+		while (fc.find()) {
+			int from = fc.start();
+			int to = fc.end();
+			System.out.println("Siècle : " + pi.substring(from,to));
+		}
 	}
-
 }
